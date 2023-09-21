f3561d86d1348bfa2e929e5f0c9f1fb0f88f2bf6
CORXYKO <florezcorxy@gmail.com>
2023-09-20 23:13:19 -0500

    comentario

--- src/main/java/com/example/homecare/repositories/IFavoritoRepository.java
+++ src/main/java/com/example/homecare/repositories/IFavoritoRepository.java
@@ -1,4 +1,12 @@
 package com.example.homecare.repositories;
 
-public interface IFavoritoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+/*comentario 
+ * 2
+*/
+
+public interface IFavoritoRepository extends JpaRepository{
 }
